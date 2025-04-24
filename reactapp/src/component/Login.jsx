5d58fadcd6fe54ac9cb5795a0209ff8e36fe0045
Vivek <vivek000@gmail.com>
2025-04-24 10:22:51 +0530

    url updated

--- reactapp/src/component/Login.jsx
+++ reactapp/src/component/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
     const password = e.target.password.value;
 
     
-      const response = await fetch("http://localhost:3008/login", {
+      const response = await fetch("https://studentapp2-1.onrender.com/login", {
         method: "POST",
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
--- reactapp/src/component/StudentAdmin.jsx
+++ reactapp/src/component/StudentAdmin.jsx
@@ -8,7 +8,7 @@ function StudentAdmin() {
     const sid = e.target.sid.value;
 
     if (sid == "*") {
-      const response = await fetch("http://localhost:3008/admin/show");
+      const response = await fetch("https://studentapp2-1.onrender.com/admin/show");
       const res = await response.json();
       console.log(res.msg);
       setStudentData(res.msg);
